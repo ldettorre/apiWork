2fd8e08eba14013bd1c48adb490001e8e598dd38
Luca DEttorre <ldettorre21@gmail.com>
2018-05-03 11:40:45 +0000

    api and button added

--- apiweather.js
+++ apiweather.js
@@ -2,31 +2,37 @@ let request = new XMLHttpRequest();
 
 function displayNicely(apiData) {
     let newData = JSON.parse(apiData);
+console.log(newData);
 
 
-
-
-    let htmlString = "<div><strong>City:</strong>" + newData.name + "</div>"
-     + "<div><strong>Current Weather: </strong>" + newData.weather[0].description + "</div>";
-    document.getElementById("data").innerHTML = htmlString
+    let htmlString = "<div><strong>City:</strong>" + newData.name + "</div>" +
+        "<div><strong>Current Weather: </strong>" + newData.weather[0].main + "</div>" + "<div><strong>Temperature in Celcius:</strong>" + (newData.main.temp -273.15).toFixed(2) + "C</div>";
+    document.getElementById("data").innerHTML = htmlString;
 }
-    
 
-   
-   
-   
-    request.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            displayNicely(this.responseText);
-        }
+
+
+
+
+function submitCity() {
+    let city =document.getElementById("cityForm")["city"].value;
+    request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=6ebea87dfc131fd5402906ce4b098ab8")
+
+    request.send();
+    return false;
+
+}
+
+
+request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        displayNicely(this.responseText);
+    }
+    if (this.readyState == 4 && this.status == 404) {
+        document.getElementById("data").innerHTML = "<strong>City not found.Please try again!</strong>";
     }
 
+    // request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=Cork&APPID=6ebea87dfc131fd5402906ce4b098ab8")
 
-    
-    
-    
-
-
-request.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=Dublin&APPID=6ebea87dfc131fd5402906ce4b098ab8")
-
-request.send();
+    // request.send();
+}
